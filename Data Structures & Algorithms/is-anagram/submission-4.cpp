b697241aa0a00ccc564e8e7e365cc605c7aafb4d
Adam Oraya <215114721+adamn-o@users.noreply.github.com>
2026-09-01 16:47:01 +0800

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> hashs;
+        unordered_map<char, int> hasht;
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        if(s.size() != t.size()) {
+            return false;
+        }
+
+        for(int i = 0; i < s.size(); i++) {
+            hashs[s[i]]++;
+            hasht[t[i]]++;
+
+            if(s[i] != t[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
